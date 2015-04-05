fc4f6f2a1a76809f7ebce9ae04802ceb6c47b571
Matic Repše <matic.repse@gmail.com>
2015-04-05 14:20:09 +0000

    Working on the nav-bar.

--- js/script.js
+++ js/script.js
@@ -338,3 +338,46 @@ jQuery(document).ready(function ($) {
 		}
 	});
 });
+/*
+google.maps.visualRefresh = true;
+
+var map;
+function initialize() {
+	var geocoder = new google.maps.Geocoder();
+	var address = $('#map-input').text();
+	var mapOptions = {
+    	zoom: 15,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP,
+     	scrollwheel: false
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+	
+  	if (geocoder) {
+      geocoder.geocode( { 'address': address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+          map.setCenter(results[0].geometry.location);
+
+            var infowindow = new google.maps.InfoWindow(
+                {
+                  content: address,
+                  map: map,
+                  position: results[0].geometry.location,
+                });
+
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map, 
+                title:address
+            }); 
+
+          } else {
+          	alert("No results found");
+          }
+        }
+      });
+	}
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+/* end google maps */
